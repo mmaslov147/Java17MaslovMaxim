a3a59f683d6ea4b86c1821919accd632fb611c04
MaslovMaxim <mmaslov1470@mail.ru>
2024-04-19 12:26:43 +0300

    changes

--- src/lesson14/hw/HWException.java
+++ src/lesson14/hw/HWException.java
@@ -8,30 +8,41 @@ public class HWException {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите размер массива: ");
-        int size = scanner.nextInt();
-
-        int[] array = new int[size];
-
-        System.out.println("Введите элементы массива:");
-        for (int i = 0; i < size; i++) {
-            System.out.print("Элемент " + (i + 1) + ": ");
-            array[i] = scanner.nextInt();
-        }
-        int sum = 0;
-
         try {
-            for (int j = 0; j <= array.length; j++) {
-                sum += array[j];
+            System.out.print("Введите размер массива: ");
+            int size = scanner.nextInt();
+
+            if (size <= 0) {
+                System.out.println("Размер массива должен быть положительным числом.");
+                return;
+            }
+
+            int[] array = new int[size];
+
+            System.out.println("Введите элементы массива:");
+            for (int i = 0; i < size; i++) {
+                System.out.print("Элемент " + (i + 1) + ": ");
+                array[i] = scanner.nextInt();
+            }
+
+            int sum = 0;
+
+            try {
+                for (int j = 0; j <= array.length; j++) {
+                    sum += array[j];
+                }
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.err.println("Handle exception: " + exception.getMessage());
+            }
+
+            if (sum > 20) {
+                System.out.println("Сумма элементов массива превысила 20");
+            } else {
+                System.out.println("Sum: " + sum);
             }
         } catch (Exception e) {
-            System.err.println("Handle exception: " + e.getMessage());
+            System.out.println("Произошла ошибка ввода данных: " + e.getMessage());
         }
-        if (sum > 20) {
-            System.out.println("Сумма элементов массива превысила 20");
-        } else
-            System.out.println("Sum: " + sum);
-
     }
 }
 
